e43cae5bcda7a774be24fbea5c5b6a3c8da200e7
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-04-19 21:33:02 +0300

    validator done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,23 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
 // Login user
-const loginUser = async (req,res)=>{
-res.json({message: 'login'})
-} 
-
-
+const loginUser = async (req, res) => {
+  res.json({ message: "login" });
+};
 
 // sign up user
-const signupUser = async(req,res)=>{
-  res.json({message: 'signup'})
-}
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 module.exports = {
   loginUser,
-  signupUser
-}
+  signupUser,
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,48 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-email:{
-  type: String,
-  require: true,
-  unique: true,
-},
-password:{
-  type: String,
-  require: true,
-},
-})
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+userSchema.statics.signup = async function (email, password) {
 
+  // Validation
+  if(!email || !password){
+    throw Error('All fields must be filled')
+  }
+  // check if the email is valid
+  if(!validator.isEmail(email)){
+    throw Error("Invalid email")
+  }
+// lowerecase uppercase , number , symbol , min 8 character
+if(!validator.isStrongPassword(password)){
+  throw Error('Password is not strong, try to combine uppercase,lowercase,number,symbol, and minimum 8 characters ')
+}
 
-module.exports = mongoose.model("User",userSchema)
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Errorr("Email already used");
+  }
+
+  //encript password or hashing
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  // create on user
+  const user = await this.create({email, password: hash})
+  return user;
+  
+};
+
+module.exports = mongoose.model("User", userSchema);
