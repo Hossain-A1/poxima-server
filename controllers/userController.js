ed30a66e6147cba90c1f867d6700a686509393c1
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-04-19 23:30:56 +0300

    JWT added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "7d" });
+};
 // Login user
 const loginUser = async (req, res) => {
   res.json({ message: "login" });
@@ -11,8 +15,12 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json(user);
-  } catch (err) {
+
+    // create Token
+    const token = createToken(user._id)
+
+    res.status(200).json({email,token});
+  } catch (error) {
     res.status(400).json({ error: err.message });
   }
 };
